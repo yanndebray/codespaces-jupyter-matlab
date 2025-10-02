54f9a52e70308c2cb8eb5f6e1ecae2450118164f
Yann Debray <128002745+yanndebray@users.noreply.github.com>
2025-10-02 21:28:09 +0000

    Pumpy's back⛽

--- mymov.m
+++
@@ -1,4 +0,0 @@
-function y = mymov(x,w)
-% moving average on a signal
-y = movmean(x, w);
-end
--- ts_shake.m
+++
@@ -1,6 +0,0 @@
-data = thingSpeakRead(1470727,Fields=1,NumPoints=100,OutputFormat='TimeTable');
-y = mymov(data.FilteredResults,10);
-data.MovingAverage  = y;
-x = data.FilteredResults;
-t = data.Timestamps;
-plot(t,x,t,y)
